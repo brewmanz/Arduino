a864e9d73d20772b0cd7790ddb79be8d03731eeb
Brewmanz(ZB15) <brewmanz@gmail.com>
2021-11-23 08:25:22 +1300

    Re-implement '.concat("(Bryan)");'

--- arduino-core/src/processing/app/BaseNoGui.java
+++ arduino-core/src/processing/app/BaseNoGui.java
@@ -43,7 +43,8 @@ public class BaseNoGui {
   /** Version string to be used for build */
   public static final int REVISION = 10817;
   /** Extended version string displayed on GUI */
-  public static final String VERSION_NAME = "1.8.17";
+  public static final String VERSION_NAME = "1.8.17"
+  .concat("(Bryan)");
   public static final String VERSION_NAME_LONG;
 
   // Current directory to use for relative paths specified on the
